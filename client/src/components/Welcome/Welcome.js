39fc349024f8de6bd405e35392d86d587ce5a335
Matt Zamora <mzamora2899@gmail.com>
2020-12-03 13:45:29 -0700

    add to welcome component switching to new branch

--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -3,8 +3,22 @@ import './Welcome.css';
 
 const Welcome = () => {
 
+
+    const redirect = () => {
+
+    }
+
     return (
-        <p>This is the Welcome Page!</p>
+        <div className='welcome'>
+            <div className='welcome__intro'>
+                <p>Welcome to Picstagram! Since you just signed up you wont be following anybody or have any followers so you won't have anything on your home feed.
+            That's ok! follow some cool cats by going to the explore page and looking at different posts! Lets Go!</p>
+            </div>
+            <div className='welcome__btn'>
+                <button onClick={redirect}>Explore!</button>
+            </div>
+        </div>
+
     )
 }
 
