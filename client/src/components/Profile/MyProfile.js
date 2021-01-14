7498321d14732a89d821c13c987edcc13905592d
Matt Zamora <mzamora2899@gmail.com>
2021-01-13 21:11:33 -0700

    fix modal

--- client/src/components/Profile/MyProfile.js
+++ client/src/components/Profile/MyProfile.js
@@ -1,100 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import { useHistory } from "react-router-dom";
 import "./MyProfile.css";
 import ProfilePost from "./ProfilePost";
 import NavBar from "../NavBar/NavBar";
 import Loading from "../Loading/Loading";
 import EditProfileModal from "../EditProfileModal/EditProfileModal";
-import Modal from "@material-ui/core/Modal";
-
-export function ProfilePhotoModal() {
-  const [open, setOpen] = useState(false);
-  const user = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
-  const [photoFile, setPhotoFile] = useState(null);
-  const userId = useSelector((state) => state.auth.id);
-  const history = useHistory();
-  const [imgPreview, setImgPreview] = useState(null);
-
-  const handleChange = (e) => {
-    setPhotoFile(e.target.files[0]);
-    setImgPreview(URL.createObjectURL(e.target.files[0]));
-  };
-
-  const postPhoto = async (formData) => {
-    //   dispatch(updateProfile(formData));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append("file", photoFile);
-    await postPhoto(formData);
-
-    setTimeout(() => {
-      history.push(`/my/profile/${userId}`);
-    }, 1000);
-  };
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const body = (
-    <>
-      <div className="upload-modal">
-        <form className="upload-form" onSubmit={handleSubmit}>
-          <p>Changle Profile Picture</p>
-          <label htmlFor="file" id="file-input">
-            <input
-              className="upload-form__input"
-              onChange={handleChange}
-              type="file"
-              name="file"
-              id="file-input"
-            />
-            Upload Photo
-          </label>
-          <div className="upload-form__btn-div">
-            <button className="upload-form__btn">Upload</button>
-            <button className="upload-form__btn">Cancel</button>
-          </div>
-        </form>
-      </div>
-    </>
-  );
-
-  return (
-    <div className="modal-btn">
-      <div className="profile__img">
-        {user.profileImg ? (
-          <img
-            className="profile__img__pic"
-            src={user.profileImg}
-            onClick={handleOpen}
-            alt=""
-          />
-        ) : (
-          <AccountCircleIcon className="profile__img__pic" />
-        )}
-      </div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-      >
-        {body}
-      </Modal>
-    </div>
-  );
-}
+import ProfilePhotoModal from "../ProfilePhotoModal/ProfilePhotoModal";
 
 const MyProfile = () => {
   const user = useSelector((state) => state.auth);
---
+++ client/src/components/ProfilePhotoModal/ProfilePhotoModal.js
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import Modal from "@material-ui/core/Modal";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import "./ProfilePhotoModal.css";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  input: {
+    display: "none",
+    color: "black",
+  },
+}));
+
+function ProfilePhotoModal() {
+  const classes = useStyles();
+
+  const [open, setOpen] = useState(false);
+  const user = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const [photoFile, setPhotoFile] = useState(null);
+  const userId = useSelector((state) => state.auth.id);
+  const history = useHistory();
+  const [imgPreview, setImgPreview] = useState(null);
+
+  const handleChange = (e) => {
+    setPhotoFile(e.target.files[0]);
+    setImgPreview(URL.createObjectURL(e.target.files[0]));
+  };
+
+  const postPhoto = async (formData) => {
+    //   dispatch(updateProfile(formData));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("file", photoFile);
+    await postPhoto(formData);
+
+    setTimeout(() => {
+      history.push(`/my/profile/${userId}`);
+    }, 1000);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (e) => {
+    e.preventDefault();
+    setOpen(false);
+  };
+
+  const body = (
+    <>
+      <div className="upload-modal">
+        <form className="upload-modal__form" onSubmit={handleSubmit}>
+          <p>Changle Profile Picture</p>
+          <input
+            accept="image/*"
+            className={classes.input}
+            id="contained-button-file"
+            multiple
+            type="file"
+          />
+
+          <div className="upload-modal__btn-div">
+            <label htmlFor="contained-button-file">
+              <span className="upload-modal__btn-span">Upload</span>
+            </label>
+            <button className="upload-modal__btn" onClick={handleClose}>
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+
+  return (
+    <div className="modal-btn">
+      <div className="profile__img">
+        {user.profileImg ? (
+          <img
+            className="profile__img__pic"
+            src={user.profileImg}
+            onClick={handleOpen}
+            alt=""
+          />
+        ) : (
+          <AccountCircleIcon className="profile__img__pic" />
+        )}
+      </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        {body}
+      </Modal>
+    </div>
+  );
+}
+
+export default ProfilePhotoModal;
